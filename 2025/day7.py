5dbf554f69a39d18e2d7bb6a97991ef7155429ec
X445 <dhrubajyoti.mandal@lgepartner.com>
2025-12-10 19:36:16 +0530

    day 7: playing with multithreading :P

--- 2025/day7.py
+++ 2025/day7.py
@@ -72,7 +72,6 @@ print("part 1:", splits)
 # part 2
 
 rows, cols = len(matrix), len(matrix[0])
-print(f"{rows, cols = }")
 
 start = (0, matrix[0].index("|"))
 
@@ -91,11 +90,7 @@ for r in range(rows):
                 if matrix[r + 1][c] == "^":
                     for dr, dc in [(1, -1), (1, 1)]:
                         r_, c_ = r + dr, c + dc
-                        if (
-                            r_ in range(rows)
-                            and c_ in range(cols)
-                            and matrix[r_][c_] == "|"
-                        ):
+                        if r_ in range(rows) and c_ in range(cols) and matrix[r_][c_] == "|":
                             adj[(r, c)].append((r_, c_))
 
                 elif matrix[r + 1][c] == "|":
@@ -111,29 +106,49 @@ print("Done!")
 
 # print(adj)
 
-def dfs(node, stop, visited, paths):
-    if node == stop:
-        paths[0] += 1
-        return
 
-    visited.add(node)
+n_leaves = len(leaves)
 
-    for neighbor in adj[node]:
-        if neighbor not in visited:
-            dfs(neighbor, stop, visited, paths)
+from functools import cache
 
-    visited.remove(node)
+def dfs_helper(stop) -> int:
+    visited = set()
+
+    @cache
+    def dfs(node, paths)->int:
+        if node == stop:
+            paths += 1
+            return paths
+
+        visited.add(node)
+
+        for neighbor in adj[node]:
+            if neighbor not in visited:
+                paths = dfs(neighbor, paths)
+
+        visited.remove(node)
+        return paths
+
+
+    return dfs(start, 0)
 
 
 timelines = 0
-n_leaves = len(leaves)
-for i, stop in enumerate(leaves):
-    print(f"computing leaf {i+1}/{n_leaves}")
-    paths = [0]
-    visited = set()
 
-    dfs(start, stop, visited, paths)
+from concurrent.futures import ThreadPoolExecutor
 
-    timelines += paths[0]
+results = []
+with ThreadPoolExecutor(max_workers=40) as executor:
+    # Submit two tasks to run in parallel
+    for i, stop in enumerate(leaves):
+        print(f"computing leaf {i+1}/{n_leaves}")
+        # timelines += dfs_helper(stop)
+        _ = executor.submit(dfs_helper, stop)
+        results.append(_)
+    
+    # executor.submit(worker, 1)
+    # executor.submit(worker, 2)
 
-print("\npart 2, timelines: ", timelines)
+print(sum([r.result() for r in results]))
+
+# print("\npart 2, timelines: ", timelines)
